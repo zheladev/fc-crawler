821535b590789dbe2e7587733deb513e52af97e4
aitorsantillana <aitor.santillana@labela.nl>
2019-05-26 00:58:40 +0200

    properly retrieve message content, along with quotes

--- fc_scrapper/spiders/thread.py
+++ fc_scrapper/spiders/thread.py
@@ -1,4 +1,7 @@
+import re
+
 from scrapy.spiders import CrawlSpider
+
 from fc_scrapper.items.post import PostItem
 from fc_scrapper.items.thread import ThreadItem
 
@@ -21,7 +24,7 @@ class ThreadSpider(CrawlSpider):
         'posted_at':
             './tr/td/a[contains(@name, "post")]/following-sibling::text()',
         'content':
-            './/div[contains(@id, "post_message_")]/following-sibling::text()'
+            './/td[contains(@id, "td_post_")]/div[@id="HOTWordsTxt"]/..'
     }
 
     start_urls = [
@@ -47,6 +50,7 @@ class ThreadSpider(CrawlSpider):
 
         for item in selector.xpath(self.posts_list_xpath):
             post = PostItem()
+            print('NEW POST')
             post['thread_id'] = thread['id_']
             for attr, xpath in self.post_item_fields.items():
                 post[attr] = self.get_attr(attr, item.xpath(xpath))
@@ -57,12 +61,52 @@ class ThreadSpider(CrawlSpider):
             'id_': lambda x: x.get()[4:],
             'user_id': lambda x: x.get().split('=')[-1:][0],
             'posted_at': lambda x: x.get()[5:-10],
-            'content': lambda x: x.get()[4:]
+            'content': lambda x: self.get_content(x)
         }
-
+        if attr == 'content':
+            # import pdb;pdb.set_trace()
+            pass
         return _lambdas[attr](xpath)
 
-    def get_content(self, attr, content):
-        #TODO: get HOTWordsTxt (citas)
-        #TODO: get text
-        pass
+
+def get_content(content):
+    # TODO: handle all possible cases
+    #   [x] 1: Only text without breaks
+    #   [x] 2: Only text with breaks
+    #   [x] 3: Quote & text w/o breaks
+    #   [x] 4: Quote & text w/ breaks
+    #   [ ] 5: Text with quote(s) in between
+    garbage_filter = re.compile('(\n|\r|\t)+')
+
+    # FIXME: Probably breaks if quote is found
+    #        in between two bodies of text
+    raw_message_lines = content.xpath(
+        './div[contains(@id, "post_message_")]'
+        '/following-sibling::text()').extract()
+    
+    raw_quotes = content.xpath(
+        './div[contains(@style, ":") and not(@id)]//text()').extract()
+    clean_quotes = list(filter(lambda x: not garbage_filter.search(x),
+                               raw_quotes))
+
+    message_lines = list(filter(bool,
+                                (map(lambda m: garbage_filter.sub('', m),
+                                     raw_message_lines))))
+
+    # print('raw data: \n', content.extract())
+    # print("quotes without text(): \n",
+    #       content.xpath('./div[contains(@style, ":") and not(@id)]')
+    #       .extract())
+    # print("\nquotes with text(): \n", clean_quotes)
+    # print("clean_message_lines \n", message_lines)
+    # print("\nmessage content: \n", content.xpath(
+    #     './div[contains(@id, "post_message_")]'
+    #     '/following-sibling::text()').extract())
+
+    # !! if quote and quote has a href to another post then there's no need
+    # to save said quote, look into having relationships in between
+    # comments
+    return {
+        'quotes': clean_quotes,
+        'message_lines': message_lines
+    }
