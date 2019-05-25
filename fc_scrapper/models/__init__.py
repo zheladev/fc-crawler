644b4f2cdd80e2169a162d690551d1c7f4843e4d
aitorsantillana <aitor.santillana@labela.nl>
2019-05-25 19:57:54 +0200

    return db engine based on settings

--- fc_scrapper/models/__init__.py
+++ fc_scrapper/models/__init__.py
@@ -4,12 +4,12 @@ from sqlalchemy.engine.url import URL
 from fc_scrapper import settings
 
 
-def get_engine():
+def get_engine(s):
     """
     Performs a database connection using specified settings
     :return: SQLAlchemy engine instance
     """
-    return create_engine(URL(**settings.DATABASE))
+    return create_engine(URL(**s))
 
 
 def db_connect():
@@ -17,4 +17,5 @@ def db_connect():
     Performs database connection using database settings from settings.py.
     Returns sqlalchemy engine instance
     """
+    return get_engine(settings.DATABASE)
 
