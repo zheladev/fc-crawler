2088737c03089a9a016596a44bd1d730f2c61e66
aitorsantillana <aitor.santillana@labela.nl>
2019-05-28 15:31:06 +0200

    run thread crawlers in parallel

--- fc_scrapper/__init__.py
+++ fc_scrapper/__init__.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from scrapy.crawler import CrawlerRunner
 from scrapy.settings import Settings
 from twisted.internet import reactor, defer
@@ -14,12 +16,23 @@ def main():
 
     @defer.inlineCallbacks
     def crawl():
+        def _crawl_threads(thread_list):
+            print('foo')
+            i = list(map(list,
+                         zip(*[iter(thread_list)] * ceil(
+                             len(thread_list) / 6))))
+            print('bar')
+            for zipped_threads in i:
+                print('starting runner...')
+                runner.crawl(ThreadSpider, start_urls=zipped_threads)
+            runner.join()
+
         threads_to_crawl = []
         print('Crawling forum for threads...')
-        yield runner.crawl(ForumSpider, threads=threads_to_crawl)
-        print('Crawling threads for posts...')
-        yield runner.crawl(ThreadSpider, start_urls=threads_to_crawl)
+        forum_spider = runner.crawl(ForumSpider, threads=threads_to_crawl)
+        forum_spider.addCallback(
+            lambda x: _crawl_threads(threads_to_crawl))
+        reactor.run()
         reactor.stop()
 
     crawl()
-    reactor.run()
